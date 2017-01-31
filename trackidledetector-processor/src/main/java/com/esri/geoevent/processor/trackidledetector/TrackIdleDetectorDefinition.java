7657e1200eeb14161fcf9093f3baf28072fa2b47
Vlad Plechnoy <vplechnoy@esri.com>
2017-01-30 17:12:32 -0800

    #1297 - Fields in the GED for the Track Idle processor do not have the timestamp and geometry tags mapped

--- trackidledetector-processor/src/main/java/com/esri/geoevent/processor/trackidledetector/TrackIdleDetectorDefinition.java
+++ trackidledetector-processor/src/main/java/com/esri/geoevent/processor/trackidledetector/TrackIdleDetectorDefinition.java
@@ -24,20 +24,15 @@
 
 package com.esri.geoevent.processor.trackidledetector;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.esri.ges.core.geoevent.DefaultFieldDefinition;
-import com.esri.ges.core.geoevent.DefaultGeoEventDefinition;
-import com.esri.ges.core.geoevent.FieldDefinition;
-import com.esri.ges.core.geoevent.FieldType;
-import com.esri.ges.core.geoevent.GeoEventDefinition;
+import com.esri.ges.core.geoevent.*;
 import com.esri.ges.core.property.LabeledValue;
 import com.esri.ges.core.property.PropertyDefinition;
 import com.esri.ges.core.property.PropertyType;
 import com.esri.ges.framework.i18n.BundleLogger;
 import com.esri.ges.framework.i18n.BundleLoggerFactory;
 import com.esri.ges.processor.GeoEventProcessorDefinitionBase;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrackIdleDetectorDefinition extends GeoEventProcessorDefinitionBase
 {
@@ -61,8 +56,8 @@ public class TrackIdleDetectorDefinition extends GeoEventProcessorDefinitionBase
 			fds.add(new DefaultFieldDefinition("trackId", FieldType.String, "TRACK_ID"));
 			fds.add(new DefaultFieldDefinition("idle", FieldType.Boolean));
 			fds.add(new DefaultFieldDefinition("idleDuration", FieldType.Double));
-			fds.add(new DefaultFieldDefinition("idleStart", FieldType.Date));
-			fds.add(new DefaultFieldDefinition("geometry", FieldType.Geometry));
+			fds.add(new DefaultFieldDefinition("idleStart", FieldType.Date, "TIME_START"));
+			fds.add(new DefaultFieldDefinition("geometry", FieldType.Geometry, "GEOMETRY"));
 			ged.setFieldDefinitions(fds);
 			geoEventDefinitions.put(ged.getName(), ged);
 		}
